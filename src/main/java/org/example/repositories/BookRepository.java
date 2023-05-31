96c507750490513befd4f9a39164d57b1909409d
amirghorbani <amirghorbanali2@gmail.com>
2023-05-31 16:10:42 +0330

    some changes in classes for exp (some methods were made public and service classes writed )

--- src/main/java/org/example/repositories/BookRepository.java
+++ src/main/java/org/example/repositories/BookRepository.java
@@ -11,30 +11,48 @@ import java.sql.SQLException;
 
 public class BookRepository {
 
-    void save(Book book) throws SQLException {
+    public void save(Book book) throws SQLException {
         Connection conn = MyConnection.Getconnection();
-        String sql = "insert into Books(BookID , BookName, PublishedYear , AuthorName)  Values ( ? , ? , ?)";
+        String sql = "insert into Books( BookName, PublishedYear , AuthorName)  Values ( ? , ? , ?)";
         PreparedStatement pstatement = conn.prepareStatement(sql);
         pstatement.setString(1, book.getBookName());
         pstatement.setInt(2, book.getPublishedYear());
         pstatement.setString(3, book.getAuthorName());
         pstatement.executeUpdate();
+//
+        String sql2 = "select AuthorId from Authors where AuthorName = ? ";
+        PreparedStatement pstatement2 = conn.prepareStatement(sql2);
+        pstatement2.setString(1 , book.getAuthorName());
+        ResultSet resultSet  =pstatement2.executeQuery();
+        int authorid = resultSet.getInt("AuthorId");
+
+        String sql3 = "select BookId from Books where BookName = ? ";
+        PreparedStatement pstatement3 = conn.prepareStatement(sql3);
+        pstatement3.setString(1 , book.getBookName());
+        ResultSet resultSet1  =pstatement3.executeQuery();
+        int bookid = resultSet1.getInt("BookId");
+
+        String sql4 = "insert into Authorbook(BookId , AuthorId) values (? , ? )";
+        PreparedStatement pstatement4 = conn.prepareStatement(sql4);
+        pstatement4.setInt(1, authorid);
+        pstatement4.setInt(2, bookid);
+        pstatement4.executeUpdate();
         conn.close();
     }
 
-    Book load(int bookid) throws SQLException {
+    public Book load(int bookid) throws SQLException {
         Connection conn = MyConnection.Getconnection();
         String sql = "select * from books where BookId = ?";
         PreparedStatement pstatement = conn.prepareStatement(sql);
         pstatement.setInt(1, bookid);
         ResultSet resultSet = pstatement.executeQuery();
         conn.close();
-        return new Book(resultSet.getInt("BookId"), resultSet.getString("BookName"),
+        return new Book( resultSet.getString("BookName"),
                 resultSet.getInt("PublishedYear"), resultSet.getString("AuthorName"));
 
     }
 
-    void delete(Book book) throws SQLException {
+    public void delete(Book book) throws SQLException {
         Connection conn = MyConnection.Getconnection();
         String sql = "Delete FROM  books WHERE BooksId = ?";
         PreparedStatement pstatement = conn.prepareStatement(sql);
