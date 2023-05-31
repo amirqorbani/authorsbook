3f68aa297c416a42d0e34cd9af3ba012fd056cee
amirghorbani <amirghorbanali2@gmail.com>
2023-05-31 18:01:11 +0330

    getter methods rewrite and this last version of project

--- src/main/java/org/example/services/BookService.java
+++ src/main/java/org/example/services/BookService.java
@@ -1,4 +1,22 @@
 package org.example.services;
 
+import org.example.MyConnection;
+import org.example.entity.Author;
+import org.example.entity.Book;
+import org.example.repositories.AuthorRepository;
+import org.example.repositories.BookRepository;
+
+import java.sql.*;
+
 public class BookService {
+  public  void  addBook(String bookname , int publishedyear , int authorid) throws SQLException {
+      Connection conn = MyConnection.Getconnection();
+      String sql = "select AuthorName from Authors where AuthorId = ? ";
+      PreparedStatement pstatement = conn.prepareStatement(sql);
+      pstatement.setInt(1 , authorid);
+      ResultSet resultSet  =pstatement.executeQuery();
+        String authorname = resultSet.getString("AuthorName");
+      new BookRepository().save(new Book(bookname , publishedyear ,authorname ));
+
+  }
 }
