dfe6e5f333b00545c376d1860253ace2edc74211
amirghorbani <amirghorbanali2@gmail.com>
2023-05-31 14:31:08 +0330

    first version of the project

--- src/main/sql/createTables.sql
+++ src/main/sql/createTables.sql
@@ -1,14 +1,14 @@
-create table books(
-    "BookId" int NOT NULL  PRIMARY KEY ,
+create table Authors(
+    "AuthorId" int NOT NULL  PRIMARY KEY ,
     "FirstName"varchar(20) not null ,
     "LastName"varchar(20) not null ,
-    "Age"varchar(20) not null );
+    "Age"int not null );
 
-create table authors(
-      "AuthorId" int NOT NULL  PRIMARY KEY ,
+create table Books(
+      "BookId" int NOT NULL  PRIMARY KEY ,
       "BookName"varchar(20) not null ,
       "PublishedYear"int  not null ,
-         "Age"int  not null
+         "AuthorName" varchar(20)  not null
     );
 create table authorbook(
     "AuthorId"int not null primary key ,
